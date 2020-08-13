06824c4c587d51607bbdc216833b741194eab6b4
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-13 13:46:03 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_echo_blocking_sam_d11_xpro
    MH3-39752
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.h
+++ apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.h
@@ -65,18 +65,14 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 16)
 #define LED_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 16)
 #define LED_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 16)
-#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 16)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 16)
 #define LED_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 16)
+#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 16)) & 0x01)
 #define LED_PIN                  PORT_PIN_PA16
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -172,6 +168,24 @@ typedef enum
 
     /* PA23 pin */
     PORT_PIN_PA23 = 23,
+
+    /* PA24 pin */
+    PORT_PIN_PA24 = 24,
+
+    /* PA25 pin */
+    PORT_PIN_PA25 = 25,
+
+    /* PA27 pin */
+    PORT_PIN_PA27 = 27,
+
+    /* PA28 pin */
+    PORT_PIN_PA28 = 28,
+
+    /* PA30 pin */
+    PORT_PIN_PA30 = 30,
+
+    /* PA31 pin */
+    PORT_PIN_PA31 = 31,
 
     /* This element should not be used in any of the PORT APIs.
      * It will be used by other modules or application to denote that none of
@@ -775,7 +789,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -814,7 +828,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
