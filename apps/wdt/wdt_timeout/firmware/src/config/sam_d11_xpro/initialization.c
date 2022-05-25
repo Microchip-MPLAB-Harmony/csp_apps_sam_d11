a2984e2bd04841ed1d70a6ada244f0bb6e452885
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-05-25 12:55:31 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_d11_xpro
    MH3-67484
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/wdt/wdt_timeout/firmware/src/config/sam_d11_xpro/initialization.c
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_d11_xpro/initialization.c
@@ -156,11 +156,10 @@ void SYS_Initialize ( void* data )
 
 
 
-    NVMCTRL_Initialize( );
-
     SERCOM2_USART_Initialize();
 
-    EVSYS_Initialize();
+    NVMCTRL_Initialize( );
+
 
 	SYSTICK_TimerInitialize();
     EIC_Initialize();
