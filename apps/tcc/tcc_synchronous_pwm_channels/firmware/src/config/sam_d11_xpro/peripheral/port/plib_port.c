89d78cab550c980cd0bafe8b4768ffe072afa45c
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-24 16:23:47 +0530

    [TCC] Regenerated demo for tcc_tcc_synchronous_pwm_channels_sam_d11_xpro
    MH3-78928

--- apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
+++ apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/sam_d11_xpro/peripheral/port/plib_port.c
@@ -71,13 +71,13 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[4] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[5] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[4] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[5] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x55;
-   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x55;
+   PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x55U;
+   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x55U;
 
 
 }
